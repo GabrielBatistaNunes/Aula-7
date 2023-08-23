e18495c2c11cf13a1ff99e43a018278f357099e7
GabrielBatistaNunes <142436040+GabrielBatistaNunes@users.noreply.github.com>
2023-08-23 17:26:47 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,103 @@
+/*let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2)
+console.log(b % 2)
+console.log(c % 2)
+console.log(d % 2)
+console.log(e % 2)
+*/
+
+//idade
+/*
+let minhaidade = 19;
+console.log("antes", minhaidade);
+minhaidade = minhaidade + 1;
+console.log("depois", minhaidade);
+*/
+/*
+let n1 = 3;
+let n2 = 4;
+let soma = n1 + n2;
+console.log("o resultado da soma de 3 + 4 é:", soma);
+
+let n3 = 3;
+let n4 = 5;
+let n5 = 2;
+let resultado = n3 * n4 / n5;
+console.log("o resultado de 3 * 5 / 2 é:", resultado);
+
+let n6 = 5;
+let n7 = 4;
+let n8 = -1;
+let resultado2 = (n6 - n7) * n8;
+console.log("o resultado de (5 - 4) * -1 é:", resultado2);
+
+// um outro jeito de se fazer, mt menor: console.log(234 % 5);
+let n9 = 234;
+let n10 = 5;
+let resultado3 = 234 % 5;
+console.log("o resultado do resto da divisão de 234 por 5 é:", resultado3);
+*/
+/*
+let n1 = 2;
+let n2 = 3;
+console.log("2 é igual a 3?", n1 === n2);
+console.log("2 é diferente de 3?", n1 !== n2);
+console.log("2 é maior que 3?", n1 > n2);
+console.log("2 é menor que 3?", n1 < n2);
+*/
+/*
+let A = 5;
+let B = 3;
+let C = 0;
+
+console.log(A === B || A !== C);
+console.log(A === B && A !== C);
+console.log("----separador----")
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+/*
+let a = true;
+let b = false;
+let c = true;
+console.log(a && b);
+console.log(b && c);
+console.log(a && c);
+console.log(a && b && c);
+
+console.log("atividade 2")
+
+console.log(a || b);
+console.log(b || c);
+console.log(a || c);
+console.log(a || b || c);
+*/
+/*
+console.log("atividade 3")
+
+let a = true;
+let b = false;
+
+console.log("era", a);
+console.log("virou",!a);
+
+console.log("era", b);
+console.log("virou", !b);
+*/
+
+let nome;
+nome = prompt("Seu nome");
+let idade;
+idade = Number(prompt("Sua idade"));
+maioridade = 18;
+console.log("seu nome é", nome);
+console.log(idade)
+console.log(idade >= maioridade);
+console.log("você terá", 27 + idade, "em 2050");
